6788f75bb30974907b35e55f69952edb89e73f94
Mehdi Ghachoui <mehdighachoui@gmail.com>
2025-02-23 19:01:51 +0000

    ...masking

--- main.go
+++ main.go
@@ -70,7 +70,6 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	screen.DrawImage(img, op)
 }
 
-// ?
 // works like zoom somehow ??
 // more meangninful when the window is resizable
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
